e550c23c3d4d4a31bbe9d5d8cc9233842cc606e3
SavaOurl <dendiroblek@gmail.com>
2023-07-29 14:59:30 +0400

    Beta-header was created

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,22 +6,35 @@ function Header() {
 
     return(
         <div className="header">
-            <div className="container">
-                <div className="header__top">
-                    <div className="header__top_elem">
-                        <div className="header__top_cityIcon"></div>
-                        <div className="header__top_tag">Ульяновск</div>
-                    </div>
-                    <div className="header__top_elem">
-                        <div className="header__top_phoneIcon"></div>
-                        <div className="header__top_tag">8 812 309-82-88</div>
-                    </div>
-                    <div className="header__top_elem">
-                        <div className="header__top_basketIcon">
-                            {/* здесь будет отображаться кол-во товаров в корзине */}
-                            <div className="header__top_basketIcon-counter">14</div> 
+            <div className="header__divider">
+                <div className="container">
+                    <div className="header__top">
+                        <div className="header__top_elem">
+                            <div className="header__top_cityIcon"></div>
+                            <div className="header__top_tag">Ульяновск</div>
                         </div>
-                        <div className="header__top_tag">В корзине (4 товара)</div>
+                        <div className="header__top_elem">
+                            <div className="header__top_phoneIcon"></div>
+                            <div className="header__top_tag">8 812 309-82-88</div>
+                        </div>
+                        <div className="header__top_elem">
+                            <div className="header__top_basketIcon">
+                                {/* здесь будет отображаться кол-во товаров в корзине */}
+                                <div className="header__top_basketIcon-counter">14</div> 
+                            </div>
+                            <div className="header__top_tag">В корзине (4 товара)</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="header__main">
+                <div className="container">
+                    <div className="header__main_wrapper">
+                            <div className="header__main_item">Доставка и оплата</div>
+                            <div className="header__main_item">Оптовые поставки</div>
+                            <div className="header__main_logo"></div>
+                            <div className="header__main_item">контакты</div>
+                            <div className="header__main_item">каталог товаров</div>
                     </div>
                 </div>
             </div>
