c6fa6f993f6a85b21a62c738da66ef45f2a83d02
SavaOurl <dendiroblek@gmail.com>
2023-08-06 11:34:28 +0400

    product card

--- src/components/pages/catalogPage/productCard/productCard.js
+++ src/components/pages/catalogPage/productCard/productCard.js
@@ -1,17 +1,16 @@
 import React from "react";
 import './ProductCard.scss';
 import cardBasket from '../../../../resources/icons/productCard/cardbasket.svg';
-const ProductCard = () => {
+const ProductCard = ({photoPath,title,price}) => {
     return(
         <>
             <div className="card__wrapper">
-                {/* Заменить на img */}
-                <div className="card__img"></div>
+                <img alt="productimg" src={`http://127.0.0.1:3004${photoPath}`} className="card__img"></img>
                 <div className="card__describe">
-                    <div    className="card__describe_name">smthing</div>
+                    <div    className="card__describe_name">${title}</div>
                 </div>
                 <div className="card__footer">
-                    <div className="card__footer_price">280 руб</div>
+                    <div className="card__footer_price">${price} руб</div>
                     <div className="card__footer_basket">
                         <img className="card__footer_basket-icon" alt="cardBasket" src={cardBasket}/>
                         <div className="card__footer_basket-title">В корзину</div>
