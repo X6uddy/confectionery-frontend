30d6db6df23c7dceed5823eb5889cb9437e19c9c
SavaOurl <dendiroblek@gmail.com>
2023-08-07 17:47:41 +0400

    refactored some code, change some field in product card for routing to the single page

--- src/components/pages/catalogPage/CatalogPage.js
+++ src/components/pages/catalogPage/CatalogPage.js
@@ -3,8 +3,9 @@ import React from "react";
 import ProductCard from "../../productCard/ProductCard";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { loadNextProducts, fetchProducts} from "../../../store/catalogSlice";
-import { useGetSumProductsQuery } from "../../../store/productsApiSlice";
+import { loadNextProducts, fetchProducts, fetchSumProducts} from "../../../store/catalogSlice";
+import Spinner from '../../spinner/Spinner';
+import error503 from '../../../resources/img/catalogPage/error503.jpg';
 
 import './CatalogPage.scss'
 
@@ -12,14 +13,14 @@ import './CatalogPage.scss'
 const CatalogPage = () => {
     
     const dispatch = useDispatch()
-    const catalogStates = useSelector(state => state.catalogStates);
-    const {data} = useGetSumProductsQuery();
+    const {dataCatalog, skip, catalogError,isLoading, sumOfProducts} = useSelector(state => state.catalogStates);
     useEffect(() => {   
-            if(catalogStates.skip === 0) {
-                dispatch(fetchProducts(catalogStates.skip))
+            if(skip === 0) {
+                dispatch(fetchProducts(skip))
                 dispatch(loadNextProducts())
+                dispatch(fetchSumProducts())
             }
-    },[catalogStates.skip,dispatch])
+    },[skip,dispatch])
 
     
     return(
@@ -33,7 +34,9 @@ const CatalogPage = () => {
                     <div className="catalog__filter_item"><span>Торты</span></div>
                 </div>
                 <div className="catalog__products-wrapper">
-                        {catalogStates.data.map((product) => {
+                    {catalogError ? <img alt="error503" style={{width: '80%'}} src={error503} /> : ''}
+                    {isLoading ? <Spinner /> : ''}
+                        {dataCatalog.map((product) => {
                             return (
                                 <div key={product._id} className="catalog__products_item">
                                     <ProductCard product={product} />
@@ -41,9 +44,9 @@ const CatalogPage = () => {
                             )
                         })} 
                 </div>
-                {!(catalogStates.skip >= data) ? <button className="catalog__btn" onClick={() => {
+                {!(skip >= sumOfProducts) ? <button className="catalog__btn" onClick={() => {
                     dispatch(loadNextProducts())
-                    dispatch(fetchProducts(catalogStates.skip))
+                    dispatch(fetchProducts(skip))
                     }}>Показать ещё</button>: ''}
             </div>  
         </div>
