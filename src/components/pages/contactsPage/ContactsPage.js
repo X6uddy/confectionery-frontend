8c27a2be0e49e8783ae54463ee2add41def76732
Volkov Sasha <sashavolkov_03@mail.ru>
2023-08-19 19:27:41 +0400

    the "Contacts" page has been made

--- src/components/pages/contactsPage/ContactsPage.js
+++ src/components/pages/contactsPage/ContactsPage.js
@@ -1,12 +1,59 @@
 import React from "react";
 
-import './ContactsPage.scss';
+import logo from '../../../resources/img/contacts/logo_img.png';
 
+import './contactsPage.scss';
+import './contactPage-media.scss';
 
 const ContactsPage = () => {
+    return (
+        <div className="container">
+            <div className="contacts">
+                <div className="contacts-logo">
+                    <img src={logo} alt="logo"/>
+                </div>
 
-    return(
-        <h1>ContactsPage</h1>
+                <div className="contacts-info">
+                    {/* <h1 className="contacts-info__title">Contacts</h1> */}
+
+                    <div className="contacts-info-block">
+                        <h3 className="contacts-info__subtitle">Режим работы:</h3>
+                        <div className="contacts-info_working">
+                            <div className="contacts-info_working-days">
+                                <p>Понедельник</p>
+                                <p>Вторник</p>
+                                <p>Среда</p>
+                                <p>Четверг</p>
+                                <p>Пятница</p>
+                                <p>Суббота</p>
+                                <p>Воскресенье</p>
+                            </div>
+                            <div className="contacts-info_working-hours">
+                                <p>7:00-17:00</p>
+                                <p>7:00-17:00</p>
+                                <p>7:00-17:00</p>
+                                <p>7:00-17:00</p>
+                                <p>Выходной</p>
+                                <p>Выходной</p>
+                                <p>7:00-17:00</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="contacts-info-block">
+                        <h3 className="contacts-info__subtitle">Пункты самовывоза:</h3>
+                        <p className="contacts-info__descr">20-й Инженерный проезд, 5</p>
+                    </div>
+
+                    <div className="contacts-info-block">
+                        <h3 className="contacts-info__subtitle">Телефоны:</h3>
+                        <p className="contacts-info__descr"><a href="tel:+79084798406">+7 (908) 479-84-06</a></p>
+                        <p className="contacts-info__descr"><a href="tel:+79084798406">+7 (908) 479-84-06</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-export default ContactsPage
+
+export default ContactsPage;
