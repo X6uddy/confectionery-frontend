4402822e8e4ecfdf497fb05aa7729349adb1d13f
Volkov Sasha <sashavolkov_03@mail.ru>
2023-08-19 14:02:40 +0400

    complete reconstruction of the slider

--- src/components/swiper/Slider.js
+++ src/components/swiper/Slider.js
@@ -21,27 +21,39 @@ import photo127 from './img/127.jpg';
 
 const Slider = () => {
 
-    const slidesViewCount = () => {
-        if(window.innerWidth > 1280) {
-            return 2
-        } else {
-            return 1
-        }
-    }
     return(
         <div className="container">
             <h2 className="slider__title">Популярные изделия</h2>
             <Swiper
-                slidesPerView={slidesViewCount()}
-                spaceBetween={0}
-                loop={true}
-                centeredSlides={true}
-                autoplay={{
-                delay: 3500, //3500
-                disableOnInteraction: false,
+                breakpoints={{
+                    300: {
+                        slidesPerView: 1, 
+                        spaceBetween: 10,
+                    },
+                    576: {
+                        slidesPerView: 1,
+                        spaceBetween: 20,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    1200: {
+                        slidesPerView: 2,
+                        spaceBetween: 30,
+                    },
+                    1920: {
+                        slidesPerView: 2,
+                        spaceBetween: 30
+                    }
                 }}
-                pagination={{
-                clickable: true,
+                loop={true}
+                speed={5000}
+                spaceBetween= {30}
+                centeredSlides={false}
+                autoplay={{
+                    delay: 100,
+                    disableOnInteraction: false,
                 }}
                 navigation={false}
                 modules={[Autoplay, Navigation]}
@@ -49,90 +61,42 @@ const Slider = () => {
             >
                 <SwiperSlide>
                     <Link to={`/catalog/${233}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo233}/>
-                        <div className="slider__descr">Пирожное "Тирамису"</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo233}/>
-                                <figcaption>
-                                    <h3>Пирожное "Тирамису"</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo233}/>
+                        <div className="slider__descr">Пирожное "Тирамису"</div>
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Link to={`/catalog/${77}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo77}/>
-                        <div className="slider__descr">Кольцо с творожной начинкой</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo77}/>
-                                <figcaption>
-                                    <h3>Кольцо с творожной начинкой</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo77}/>
+                        <div className="slider__descr">Кольцо с творожной начинкой</div>
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Link to={`/catalog/${62}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo62}/>
-                        <div className="slider__descr">"Творожный с изюмом"</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo62}/>
-                                <figcaption>
-                                    <h3>"Творожный с изюмом"</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>                        
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo62}/>
+                        <div className="slider__descr">"Творожный с изюмом"</div>       
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Link to={`/catalog/${73}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo73}/>
-                        <div className="slider__descr">"Язык с сахаром"</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo73}/>
-                                <figcaption>
-                                    <h3>"Язык с сахаром"</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo73}/>
+                        <div className="slider__descr">"Язык с сахаром"</div>
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Link to={`/catalog/${300}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo300}/>
-                        <div className="slider__descr">"Безе воздушное"</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo300}/>
-                                <figcaption>
-                                    <h3>"Безе воздушное"</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo300}/>
+                        <div className="slider__descr">"Безе воздушное"</div>
                     </Link>
                 </SwiperSlide>
                 <SwiperSlide>
                     <Link to={`/catalog/${127}`}>
-                        {/* <img className="slider__photo" alt="sliderPhoto"  src={photo127}/>
-                        <div className="slider__descr">"Рогалик с варёной сгущенкой"</div> */}
-                        <div className="slider-block">
-                            <figure className="slider-wrapper">
-                                <img className="slider__photo" alt="sliderPhoto"  src={photo127}/>
-                                <figcaption>
-                                    <h3>"Рогалик с варёной сгущенкой"</h3>
-                                </figcaption>			
-                            </figure>
-                        </div>
+                        <img className="slider__photo" alt="sliderPhoto"  src={photo127}/>
+                        <div className="slider__descr">"Рогалик с варёной сгущенкой"</div>
                     </Link>
                 </SwiperSlide>
             </Swiper>
         </div>
     )
 }
-export default Slider
+export default Slider;
