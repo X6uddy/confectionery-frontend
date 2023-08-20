357218fad6ffc7993d2a58679f8c6407ba6efff6
Volkov Sasha <sashavolkov_03@mail.ru>
2023-08-20 19:17:31 +0400

    added section promo(demo version)

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,11 +4,25 @@ import Slider from '../swiper/Slider';
 import Reasons from '../reasons/Reasons';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
+import promoImg from '../../resources/img/main_page/promo.jpg'
+
 
 function MainPage() {
 
     return(
         <>
+            <div className="promo">
+                <div className="container">
+                    <h1 className="promo__title">
+                        Приветствуем вас на виртуальных просторах кондитерской фабрики "ИП Курушин О.А"
+                    </h1>
+                    <p className="promo__descr">
+                        На протяжении многих лет наша фабрика с любовью и страстью создает настоящие произведения искусства в мире кондитерских изделий. 
+                        Каждый десерт, каждое пирожное – это результат непрерывных исследований, слияния традиций и новаторских подходов. 
+                        Мы ставим перед собой цель – не только порадовать ваши вкусовые рецепторы, но и создать впечатление, которое будет оставаться с вами на долгие годы.
+                    </p>
+                </div>
+            </div>
             <ErrorBoundary>
                 <Slider/>
             </ErrorBoundary>
