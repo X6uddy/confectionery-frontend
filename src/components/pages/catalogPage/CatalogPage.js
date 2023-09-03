98667892e96da952ec66f8b006286ee26c23e26c
SavaOurl <dendiroblek@gmail.com>
2023-09-03 18:50:02 +0400

    deleted filter from catalog page

--- src/components/pages/catalogPage/CatalogPage.js
+++ src/components/pages/catalogPage/CatalogPage.js
@@ -49,12 +49,12 @@ const CatalogPage = () => {
         <div className="catalog">
             <div className="container">
                 <div className="catalog__title">Каталог товаров</div>
-                <div className="catalog__filter-wrapper">
+                {/* <div className="catalog__filter-wrapper">
                     <div className="catalog__filter_item"><span>Кексы</span></div>
                     <div className="catalog__filter_item"><span>Рогалики</span></div>
                     <div className="catalog__filter_item"><span>Сдобное</span></div>
                     <div className="catalog__filter_item"><span>Торты</span></div>
-                </div>
+                </div> */}
                 <div className="catalog__products-wrapper">
                      <img
                         alt="arrowUp" 
