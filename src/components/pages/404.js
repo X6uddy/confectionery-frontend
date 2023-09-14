500bb01e989f2ed01c3aa9d4395fe7895feea3e0
Volkov Sasha <sashavolkov_03@mail.ru>
2023-09-14 18:18:57 +0400

    fixed paddings

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -4,7 +4,7 @@ import ErrorMessage from "../errorMessage/ErrorMessage";
 
 const Page404 = () => {
     return (
-        <div>
+        <div style={{'padding-bottom': '100px'}}>
             <ErrorMessage/>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '30px', 'marginTop': '20px'}} to="/">Back to main page</Link>
