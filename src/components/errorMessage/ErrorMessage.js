6eb6dba49919531202135f8222f6a4a035c953fd
Volkov Sasha <sashavolkov_03@mail.ru>
2023-09-18 20:51:07 +0400

    styles changed

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -2,7 +2,7 @@ import img from './error.gif'
 
 const ErrorMessage = () => {
     return (
-        <img style = {{display: 'block', width: '300px', height: '280px', objectFit: 'contain', margin: '0 auto', 'marginTop': '50px'}} src={img} alt='error'/>
+        <img style = {{display: 'block', width: '100%', height: '280px', objectFit: 'contain', margin: '0 auto', 'marginTop': '50px'}} src={img} alt='error'/>
     )
 }
 export default ErrorMessage;
