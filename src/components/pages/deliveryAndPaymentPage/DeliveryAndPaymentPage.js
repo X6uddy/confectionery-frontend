dfcf4a45d71b1a9535f0866baefaebceccf9dd67
SavaOurl <dendiroblek@gmail.com>
2023-09-19 21:34:29 +0400

    remade delivery page and changed icon

--- src/components/pages/deliveryAndPaymentPage/DeliveryAndPaymentPage.js
+++ src/components/pages/deliveryAndPaymentPage/DeliveryAndPaymentPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import bicycle from '../../../resources/img/DeliveryAndPaymentPage/bicycle.png';
+import bicycle from '../../../resources/img/DeliveryAndPaymentPage/truck.svg';
 import logo from '../../../resources/img/DeliveryAndPaymentPage/logo_img.png';
 import YandexMapDelivery from './yandexMapDelivery';
 
@@ -13,32 +13,33 @@ const DeliveryAndPaymentPage = () => {
     return (
         <div className="delivery">
             <div className="container">
-                <div className="delivery-block">
-                    <div className="delivery-block_title">
-                        <h2 className="delivery-block__title">Доставка</h2>
-                    </div>
-                        <div className="delivery-block_image"><img src={bicycle} alt="bicycle" /></div>
-                        <div className="delivery-block_descr">
-                            <p> 
-                            Мы ценим ваш интерес к нашим продуктам и стремимся обеспечить вас наилучшим обслуживанием.<br/><br/>
-                            При оформлении заказа важно учитывать количество товаров, которое вы планируете приобрести.
-                            Это связано с нашей политикой доставки, которая зависит от объема вашего заказа.<br />
-                            </p>
+                <div className="delivery__block delivery__block_truck">
+                        <div className="delivery__block_truck-image"><img src={bicycle} alt="bicycle" /></div>
+                        <div className='delivery__block_text'>
+                            <div className="delivery__block_title">Доставка</div>
+                            <div className="delivery__block_descr">
+                                <p> 
+                                Мы ценим ваш интерес к нашим продуктам и стремимся обеспечить вас наилучшим обслуживанием.<br/><br/>
+                                При оформлении заказа важно учитывать количество товаров, которое вы планируете приобрести.
+                                Это связано с нашей политикой доставки, которая зависит от объема вашего заказа.<br />
+                                </p>
+                            </div>
                         </div>
                 </div>
 
 
-                <div className="delivery-block">
-                    <div className="delivery-block__descr">
-                        <h3 className="delivery-block__info">Самовывоз</h3>
-                        <p>
-                        Вы можете сделать заказ и забрать его самостоятельно с нашего производства по адресу: 20-й Инженерный проезд, 5.<br /><br />
-                        Забрать заказ можно на следующий день в пункте самовывоза в любое удобное время в промежутке с 8 до 17 часов.
-                        </p>
+                <div className="delivery__block">
+                    <div className='delivery__block_text'>
+                        <div className="delivery__block_title">Самовывоз</div>
+                        <div className="delivery__block_descr">
+                            <p>
+                            Вы можете сделать заказ и забрать его самостоятельно с нашего производства по адресу: 20-й Инженерный проезд, 5.<br /><br />
+                            Забрать заказ можно на следующий день в пункте самовывоза в любое удобное время в промежутке с 8 до 17 часов.
+                            </p>
+                        </div>
                     </div>
-                    <div className="delivery-block__image"><img src={logo} alt="logo" /></div>
+                    <div className="delivery__block_image"><img src={logo} alt="logo" /></div>
                 </div>
-
                 <YandexMapDelivery />
             </div>
         </div>
